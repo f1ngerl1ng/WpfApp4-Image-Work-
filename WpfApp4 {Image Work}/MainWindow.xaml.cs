591b78eb664a370e7eeb23efa6e159db8b0489aa
f1ngerl1ng <dimonik09@gmail.com>
2019-06-04 09:17:29 +0300

    Створення нового проекту по роботі з зображеннями!

---
+++ WpfApp4 {Image Work}/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using ImageWorks.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4__Image_Work_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) " +
+                "| *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            if (dlg.ShowDialog() == true)
+            {
+                try
+                {
+                    var imageFolderSave = "uploads";
+                    var filePath = dlg.FileName;
+                    var image = System.Drawing.Image.FromFile(dlg.FileName);
+                    if (!Directory.Exists(imageFolderSave))
+                    {
+                        Directory.CreateDirectory(imageFolderSave);
+                    }
+                    //вхідне фото для обробки
+                    var bmpOrigin = new System.Drawing.Bitmap(image);
+
+
+                    var imageName = Guid.NewGuid().ToString() + ".jpg";
+                    var imageSave = ImageWorker.CreateImage(bmpOrigin, 200, 200);
+                    
+                    if (imageSave == null)
+                        throw new Exception("Проблема обробки фото");
+
+                    var imageSaveEnd = System.IO.Path.Combine(imageFolderSave, imageName);
+                    imageSave.Save(imageSaveEnd, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Щось пішло не так {ex.Message}");
+                }
+
+            }
+        }
+    }
+}
